66d37ac881057bc5182579660c02717c36775106
JustAGhosT <smit.jurie@gmail.com>
2025-12-14 20:35:53 +0200

    fix: add routing for Scenarios and Media pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
+import ScenariosPage from './pages/ScenariosPage';
+import MediaPage from './pages/MediaPage';
 import ProtectedRoute from './ProtectedRoute';
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
         >
           <Route index element={<Navigate to="/admin" replace />} />
           <Route path="admin" element={<DashboardPage />} />
+          <Route path="admin/scenarios" element={<ScenariosPage />} />
+          <Route path="admin/media" element={<MediaPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
